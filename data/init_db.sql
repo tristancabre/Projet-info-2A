fe4f7aa0f9dda3bc2d83e0359a43a3ba4977c8c5
tristancabre <tristan.cabre@eleve.ensai.fr>
2026-09-25 13:28:02 +0000

    qq corrections d'erreurs

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,14 +1,15 @@
-CREATE SCHEMA project;
+CREATE SCHEMA IF NOT EXISTS project;
 
 -----------------------------------------------------
 -- User
 -----------------------------------------------------
+
 DROP TABLE IF EXISTS project.user CASCADE;
 CREATE TABLE project.user (
-    id_user    SERIAL PRIMARY KEY,
-    username     VARCHAR(30) UNIQUE,
-    password     VARCHAR(256),
-    email        VARCHAR(50) UNIQUE,
+    id_user      SERIAL PRIMARY KEY,
+    username     VARCHAR(30) UNIQUE NOT NULL,
+    password     VARCHAR(256) NOT NULL,
+    email        VARCHAR(50) NOT NULL,
     access_token VARCHAR(255)
 );
 
@@ -19,7 +20,7 @@ CREATE TABLE project.user (
 DROP TABLE IF EXISTS project.neo CASCADE;
 CREATE TABLE project.neo (
     id_neo      SERIAL PRIMARY KEY,
-    name        VARCHAR(255),
+    name        VARCHAR(255) NOT NULL,
     size        INT,
     distance    FLOAT,
     speed       FLOAT,
@@ -35,8 +36,8 @@ CREATE TABLE project.neo (
 DROP TABLE IF EXISTS project.favorites CASCADE;
 CREATE TABLE project.favorites (
     id_favorite SERIAL PRIMARY KEY,
-    id_user INTEGER REFERENCES project.user(id_user),
-    id_neo INTEGER REFERENCES project.neo(id_neo),
+    id_user     INTEGER REFERENCES project.user(id_user) ON DELETE CASCADE,
+    id_neo      INTEGER REFERENCES project.neo(id_neo) ON DELETE CASCADE,
     date_added  DATE
 );
 
@@ -46,14 +47,14 @@ CREATE TABLE project.favorites (
 
 DROP TABLE IF EXISTS project.alert CASCADE;
 CREATE TABLE project.alert (
-    id_alert SERIAL PRIMARY KEY,
-    id_user INTEGER REFERENCES project.user(id_user),
-    id_neo INTEGER REFERENCES project.neo(id_neo),
-    min_size    INT,
-    max_size    INT,
-    min_distance   FLOAT,
-    max_distance   FLOAT,
-    min_speed    FLOAT,
+    id_alert     SERIAL PRIMARY KEY,
+    id_user      INTEGER REFERENCES project.user(id_user) ON DELETE CASCADE,
+    id_neo       INTEGER REFERENCES project.neo(id_neo) ON DELETE CASCADE,
+    min_size     INT,
+    max_size     INT,
+    min_distance FLOAT,
+    max_distance FLOAT,
+    min_speed    FLOAT,   
     max_speed    FLOAT
 );
 
@@ -64,9 +65,9 @@ CREATE TABLE project.alert (
 DROP TABLE IF EXISTS project.neodistancehistory CASCADE;
 CREATE TABLE project.neodistancehistory (
     id_distance_history SERIAL PRIMARY KEY,
-    id_neo INTEGER REFERENCES project.neo(id_neo),
-    distance    FLOAT,
-    observation_date  DATE
+    id_neo             INTEGER REFERENCES project.neo(id_neo) ON DELETE CASCADE,
+    distance            FLOAT,
+    observation_date    DATE
 );
 
 -----------------------------------------------------
@@ -75,9 +76,9 @@ CREATE TABLE project.neodistancehistory (
 
 DROP TABLE IF EXISTS project.connectionlog CASCADE;
 CREATE TABLE project.connectionlog (
-    id_connection SERIAL PRIMARY KEY,
-    id_user INTEGER REFERENCES project.user(id_user),
-    connection_moment   TIMESTAMP
+    id_connection     SERIAL PRIMARY KEY,
+    id_user           INTEGER REFERENCES project.user(id_user) ON DELETE CASCADE,
+    connection_moment TIMESTAMP
 );
 
 -----------------------------------------------------
@@ -86,8 +87,8 @@ CREATE TABLE project.connectionlog (
 
 DROP TABLE IF EXISTS project.searchhistory CASCADE;
 CREATE TABLE project.searchhistory (
-    id_search SERIAL PRIMARY KEY,
-    id_user INTEGER REFERENCES project.user(id_user),
-    search_query    VARCHAR(255),
-    search_moment   TIMESTAMP
+    id_search     SERIAL PRIMARY KEY,
+    id_user       INTEGER REFERENCES project.user(id_user) ON DELETE CASCADE,
+    search_query  VARCHAR(255),
+    search_moment TIMESTAMP
 );
