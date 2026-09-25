c28e4b0d856c6a09a37d3ed92fe011778fd91ba0
CausseG <gabrielle.causse@eleve.ensai.fr>
2026-09-25 14:15:36 +0000

    Favorite + NeoDHist Business Object

--- backend/src/business_object/favorite.py
+++ backend/src/business_object/favorite.py
@@ -1,17 +1,36 @@
-from business_object import Neo 
-from datetime import datetime
+from datetime import date
+
+from business_object import Neo
 
 
 class Favorites:
-    def __init__(self, date_added:date , distance_history: list):
-        self.date_added = date_added,
-        self.distance_history = distance_history
-    
+    """A collection of favorited Neo objects."""
 
-    def add_neo(self, Neo)->bool:
+    def __init__(self):
+        self.neos = []
+        self.date_added = {}
+        self.distance_history = {}
 
+    def add_neo(self, neo: Neo) -> bool:
+        """Add a Neo to favorites. Returns False if already present."""
+        if neo in self.neos:
+            return False
+        self.neos.append(neo)
+        self.date_added[neo.id_neo] = date.today()
+        self.distance_history[neo.id_neo] = [neo.distance]
+        return True
 
+    def remove_neo(self, neo: Neo) -> bool:
+        """Remove a Neo from favorites. Returns False if not present."""
+        if neo not in self.neos:
+            return False
+        self.neos.remove(neo)
+        self.date_added.pop(neo.id_neo, None)
+        self.distance_history.pop(neo.id_neo, None)
+        return True
 
-    def remove_neo(self, Neo) -> bool:
-    def get_distance_history(self) -> list:
-
+    def get_distance_history(self, neo: Neo = None) -> list:
+        """Return distance history for a specific Neo, or the whole mapping if none given."""
+        if neo is not None:
+            return self.distance_history.get(neo.id_neo, [])
+        return self.distance_history
---
+++ backend/src/business_object/neo_distance_history.py
@@ -0,0 +1,20 @@
+import itertools
+from datetime import date
+
+
+class NeoDistanceHistory:
+    """The information about distance for a Neo  at a specific time."""
+
+    _id_counter = itertools.count(1)
+
+    def __init__(self, id_neo: int, observation_date: date, distance: int):
+        self.id_history = next(NeoDistanceHistory._id_counter)
+        self.id_neo = id_neo
+        self.observation_date = observation_date
+        self.distance = distance
+
+    def __str__(self):
+        return f"NeoDistanceHistory(id={self.id_history}, neo={self.id_neo}, date={self.observation_date}, distance={self.distance})"
+
+    def as_list(self) -> list:
+        return [self.id_history, self.id_neo, str(self.observation_date), self.distance]
