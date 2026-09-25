afb2592e5f540f03b2bd21753e9b3217f5eb4fc0
CausseG <gabrielle.causse@eleve.ensai.fr>
2026-09-25 13:31:35 +0000

    class favorite in progress

---
+++ backend/src/business_object/favorite.py
@@ -0,0 +1,17 @@
+from business_object import Neo 
+from datetime import datetime
+
+
+class Favorites:
+    def __init__(self, date_added:date , distance_history: list):
+        self.date_added = date_added,
+        self.distance_history = distance_history
+    
+
+    def add_neo(self, Neo)->bool:
+
+
+
+    def remove_neo(self, Neo) -> bool:
+    def get_distance_history(self) -> list:
+
